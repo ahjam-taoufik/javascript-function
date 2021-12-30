a3ef770aac2d4d19c05855115e3fc174f515be71
taoufik ahjam <taoufikahjam@gmail.com>
2021-12-30 13:12:14 +0100

    some and every

---
+++ 2-some every.js
@@ -0,0 +1,44 @@
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+console.log('function javascript')
+
+
+const tab1= [20,8,10,7,12,5,15]
+
+
+
+
+// function isEven(n){
+//     return n%2===0
+// }
+
+
+
+// function isOdd(n){
+//     return !(n%2===0)
+// }
+
+
+
+
+
+//res=tab1.every(isEven)
+ 
+
+
+
+// res=tab1.every(function(item){
+//       return item%2===0
+//     })
+//    console.log(res)
+
+
+
+// res=tab1.some(function(item){
+//       return item>4
+//     })
+//    console.log(res)
+
+
+
